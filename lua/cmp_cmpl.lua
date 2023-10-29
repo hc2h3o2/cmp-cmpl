e2f928c0b496ee61bda9c398a7eef99c66bbba22
Karol Jakusz-Gostomski <karol.jakusz.gostomski@gmail.com>
2023-10-29 10:52:55 +0100

    working

--- lua/cmp_cmpl.lua
+++ lua/cmp_cmpl.lua
@@ -1,72 +1,42 @@
 local source = {}
 
--- TODO: not working, it was but failed.....
 local create_job = function(self)
   local id = vim.fn.jobstart({ "cmpl-n" }, {
     on_stdout = function(_, data)
-      -- print(data)
       for _, line in ipairs(data) do
-        -- print(line)
-        if line == "" and self.callback ~= nil then
+        if line ~= "" then
           local complete_items = {}
-          -- for _, item in ipairs(self.output_buffer) do
-          --   local index = item:find("\t")
-          --   if index ~= nil then
-          --     local label = item:sub(0, index - 1)
-          --     local detail = item:sub(index + 1, item:len())
-          --     local kind = 12
-          --     if string.find(detail, "^Executable") then
-          --       kind = 3
-          --     elseif string.find(label, "^-") then
-          --       kind = 14
-          --     end
-          --     table.insert(complete_items, {
-          --       label = label,
-          --       kind = kind,
-          --       detail = detail,
-          --     })
-          --   end
-          -- end
-          print("Print sprzed else")
-          self.callback(complete_items)
-          self.callback = nil
-          self.output_buffer = {}
-        else
-          print("Print z else")
-          -- print(line)
-          table.insert(self.output_buffer, line)
+          local jitems = vim.fn.json_decode(line)
+          if jitems ~= nil then
+            for _, jitem in ipairs(jitems) do
+              -- print(jitem)
+              -- for key, value in pairs(jitem) do
+              --   print('\t', key, value)
+              -- end
 
-          for _, item in ipairs(self.output_buffer) do
-            local jitems = vim.fn.json_decode(item)
-            if jitems ~= nil then
-              for index, data in ipairs(jitems) do
-                print(data)
-                for key, value in pairs(data) do
-                  print('\t', key, value)
-                end
-                -- for jitem in jitems do
-                --   print("jitem")
-                --   print(jitem)
-                --   -- local index = item:find("\t")
-                --   -- if index ~= nil then
-                --   --   local label = item:sub(0, index - 1)
-                --   --   local detail = item:sub(index + 1, item:len())
-                --   --   local kind = 12
-                --   --   if string.find(detail, "^Executable") then
-                --   --     kind = 3
-                --   --   elseif string.find(label, "^-") then
-                --   --     kind = 14
-                --   --   end
-                --   --   table.insert(complete_items, {
-                --   --     label = label,
-                --   --     kind = kind,
-                --   --     detail = detail,
-                --   --   })
-                --   -- end
-                -- end
+              -- info
+              -- word
+              -- menu
+              -- abbr
+              -- icon
+              -- kind
+              -- print("jitem")
+              -- print(jitem)
+
+              local kind = 12
+              if jitem.kind == "dict" then
+                kind = 1
+              elseif jitem.kind == "tag" then
+                kind = 14
               end
+              table.insert(complete_items, {
+                label = jitem.word,
+                kind = kind,
+                detail = jitem.menu,
+              })
             end
           end
+          self.callback(complete_items)
         end
       end
     end,
@@ -81,14 +51,14 @@ source.new = function()
   local self = setmetatable({}, {
     __index = source,
   })
-  self.output_buffer = {}
+  -- self.output_buffer = {}
   self.cmpl_job = create_job(self)
   return self
 end
 
 source.reset = function(self)
   vim.fn.jobstop(self.cmpl_job)
-  self.output_buffer = {}
+  -- self.output_buffer = {}
   self.cmpl_job = create_job(self)
 end
 
@@ -138,15 +108,12 @@ end
 
 
 source.complete = function(self, params, callback)
-  -- local word = string.sub(params.context.cursor_before_line, params.offset)
   local word = params.context.cursor_before_line
   if string.sub(word, -1) ~= "." then
     for k in string.gmatch(word, "%.?([%w_-]+)$") do
       word = k
     end
-    -- print(word)
 
-    self.output_buffer = {}
     vim.fn.chansend(self.cmpl_job, self.build_input(word))
     self.callback = callback
   end
